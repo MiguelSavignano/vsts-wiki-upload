7d95a49f00dc842c6de55c6d1f4c7a9792efbb42
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-08-18 00:19:15 +0200

    covertura report

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
+  coverageReporters: ['json', 'lcov', 'text', 'cobertura'],
   reporters: ['default', 'jest-junit'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
